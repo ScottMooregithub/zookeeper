cd19c8215810b190049912703c50ac401c876668
Scott Moore <sm91srjc@gmail.com>
2022-05-23 16:32:15 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const { animals } = require("./data/animals");
 
 const express = require("express");
-const req = require("express/lib/request");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
